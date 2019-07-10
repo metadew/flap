38d80815c2b5b39c9fe51451e69a052d90c77f74
metadew <36988432+metadew@users.noreply.github.com>
2019-07-10 14:29:27 +0200

    create flap-core project

---
+++ core/java/flap-core/src/main/java/io/metadew/flap/core/App.java
@@ -0,0 +1,13 @@
+package io.metadew.flap.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
